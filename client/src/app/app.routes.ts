690bee565df86c9ac46bd8cf45082fe358248553
Filipe <filipemarquesferrao@gmail.com>
2024-07-31 10:21:03 +0100

    added auth guards, bit of styling

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,12 +4,21 @@ import { MembersListComponent } from './members/members-list/members-list.compon
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
+import { authGuard } from './_guards/auth.guard';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'lists', component: ListsComponent},
-    {path: 'members', component: MembersListComponent},
-    {path: 'members/:id', component: MemberDetailComponent},
-    {path: 'messages', component: MessagesComponent},
+
+    {
+        path: '', runGuardsAndResolvers: 'always',
+        canActivate: [authGuard],
+        children: [
+            {path: 'lists', component: ListsComponent},
+            {path: 'members', component: MembersListComponent},
+            {path: 'members/:id', component: MemberDetailComponent},
+            {path: 'messages', component: MessagesComponent},
+        ]
+    },
+
     {path: '**', component: HomeComponent, pathMatch: 'full'},
 ];
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,20 +1,23 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 import { AccountService } from '../_services/account.service';
-import { NgIf } from '@angular/common';
+import { NgIf, TitleCasePipe } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown'
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Positioning } from 'ngx-bootstrap/positioning';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, NgIf, BsDropdownModule, RouterLink, RouterLinkActive],
+  imports: [FormsModule, NgIf, BsDropdownModule, RouterLink, RouterLinkActive, TitleCasePipe],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
   accountService = inject(AccountService);
   private router = inject(Router);
+  private toastr = inject(ToastrService);
   model: any = {};
 
   login() {
@@ -22,7 +25,7 @@ export class NavComponent {
       next: _ => {
         this.router.navigateByUrl('members/')
       },
-      error: error => console.log(error)
+      error: error => this.toastr.error(error.error, 'Oops' ,{positionClass: 'toast-bottom-right'})
     })
   }
 
